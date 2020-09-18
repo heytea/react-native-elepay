d99bd06c03fa900bd64b19f135e081aea97d15aa
程广 <chengguang@heytea.com>
2020-09-18 18:04:59 +0800

    update: fix ts bug

--- index.d.ts
+++ index.d.ts
@@ -27,6 +27,6 @@ export type ElepayCallBack = (result: IElepayResult, error?: IElepayError) => vo
 
 export function initElepay(config: IElepayConfig): void
 export function handlePaymentWithPayload(payload: string, callback: ElepayCallBack): void
-export function changeLanguage(value:{ languageKey?: LANGUAGE_KEY } = { languageKey: ''}): void
+export function changeLanguage(value:{ languageKey?: Language_Key }): void
 export function handleOpenUrlString(url: string): void
 
